49b76a07688fbba506e2e5154fa3f3e16fc15633
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-25 15:38:04 +0000

    program to create class

---
+++ Createclass/src/com/stackroute/Main.java
@@ -0,0 +1,14 @@
+package com.stackroute;
+
+public class Main {
+
+    public static class Student{
+        String name;
+        int roll_no;
+    }
+    public static void main(String[] args) {
+	Student s = new Student();
+	s.name= "John";
+	s.roll_no = 2;
+    }
+}
